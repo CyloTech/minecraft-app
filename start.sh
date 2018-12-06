84c7b31251f0c946baffb3136cedf427ecedb6c0
Mylo <gavin@cylo.io>
2018-12-06 11:19:48 +0000

    Updated start directory.

--- start.sh
+++ start.sh
@@ -5,11 +5,12 @@ if [ ! -f /etc/mc_installed ]; then
     mkdir /home/minecraft
     mv /home/mcmyadmin/* /home/minecraft/
 
+    cd /home/minecraft
     /bin/sh /home/minecraft/MCMA2_Linux_x86_64 -nonotice -updateonly
 
     touch /etc/mc_installed
     curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST "https://api.cylo.io/v1/apps/installed/$INSTANCE_ID"
 fi
 
-
+cd /home/minecraft
 /home/minecraft/MCMA2_Linux_x86_64 -setpass "$ADMIN_PASS" +java.memory $JAVA_MEMORY
